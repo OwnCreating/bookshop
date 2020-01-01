95c3fea1deb33a8d99546be44339cc314db9e9a0
bobocodex57 <bobo.codex@gmail.com>
2020-01-01 22:33:54 +0630

    feature/bobo57 - update

--- resources/views/frontend/layouts/template.blade.php
+++ resources/views/frontend/layouts/template.blade.php
@@ -92,7 +92,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-color">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+            <p class="m-0 text-center text-white">Copyright &copy; Book Shop Website 2019</p>
         </div>
         <!-- /.container -->
     </footer>
